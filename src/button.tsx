85a2adf7cdc5a2daa8a7948fb93c94b466352cfe
Yudu Ban <banyudu@innobuddy.com>
2018-10-18 19:09:46 +0800

    add .npmignore

--- src/button.tsx
+++ src/button.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 
 export default class Button extends React.Component {
   public render () {
-    return (<div />)
+    return (<div>apollon-mobile-components button works</div>)
   }
 }
