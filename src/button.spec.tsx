f0671a18a44372a70ec9889886ccf13523990d1c
Yudu Ban <banyudu@innobuddy.com>
2018-10-24 18:12:16 +0800

    remove button component

--- src/button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as renderer from 'react-test-renderer'
-import Button from './button'
-
-test('button', () => {
-  const testRenderer = renderer.create(<Button/>)
-  const testInstance = testRenderer.root
-  expect(testInstance.type).toBe(Button)
-})
