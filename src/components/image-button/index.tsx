6e8abc7966f1519b0953ab21163e449abbecc99d
gx <gaoxiang@innobuddy.com>
2018-10-24 16:51:20 +0800

    add image button

---
+++ src/components/image-button/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import './image-button.scss'
+
+export interface ImageButtonPropsInterface {
+  text: string,
+  index: number,
+  onClick?: React.MouseEventHandler<HTMLElement>,
+  isCurrentSite: boolean
+}
+
+const images: string[] = ['building', 'desk', 'hallway', 'homework', 'playground']
+
+class ImageButton extends React.Component<ImageButtonPropsInterface, any> {
+  render () {
+    const { text, index, onClick, isCurrentSite } = this.props
+    console.log('fafafa', `url(~images/image-buttons/${images[index % images.length]}.png)`)
+    return (
+      <div
+        className='image-button-container'
+        style={{ backgroundImage: `url(images/image-buttons/${images[index % images.length]}.png)` }}
+        onClick={onClick}
+      >
+        <p>{text}</p>
+        {isCurrentSite && <span className='note'>当前站点</span>}
+      </div>
+    )
+  }
+}
+
+export default ImageButton
