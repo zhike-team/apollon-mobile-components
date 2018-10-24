884bfb955600942a2bd60fa6f52c8da25dd6fea1
jy <jiangyue@innobuddy.com>
2018-10-24 16:54:10 +0800

    refs #SMART-4141 use mui

--- src/report-list-item/report-list-item.story.tsx
+++ src/report-list-item/report-list-item.story.tsx
@@ -2,17 +2,19 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import ReportListItem from './report-list-item'
 
-storiesOf('日报列表item', module)
-  .add('一行文字', () => (
+storiesOf('报告列表项', module)
+  .add('日报项', () => (
     <ReportListItem
       icon='日报'
       titleOne='2018年10月18日教学服务日报'
+      handleReportDetail={() => console.log('日报')}
     />
   ))
-  .add('两行文字', () => (
+  .add('周报项', () => (
     <ReportListItem
       icon='周报'
       titleOne='2018.10.01-2018.10.07'
       titleTwo='教学服务周报'
+      handleReportDetail={() => console.log('周报')}
     />
   ))
