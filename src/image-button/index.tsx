da1b4c74821ed4f5db717139c4e902e5d983b151
Yudu Ban <banyudu@innobuddy.com>
2018-10-24 18:22:08 +0800

    adjust folder

--- src/image-button/index.tsx
+++ src/image-button/index.tsx
@@ -3,20 +3,16 @@ import './image-button.scss'
 
 export interface ImageButtonPropsInterface {
   text: string,
-  index: number,
   onClick?: React.MouseEventHandler<HTMLElement>,
   isCurrentSite: boolean
 }
 
-const images: string[] = ['building', 'desk', 'hallway', 'homework', 'playground']
-
 class ImageButton extends React.Component<ImageButtonPropsInterface, any> {
   render () {
-    const { text, index, onClick, isCurrentSite } = this.props
+    const { text, onClick, isCurrentSite } = this.props
     return (
       <div
         className='image-button-container'
-        style={{ backgroundImage: `url(images/image-buttons/${images[index % images.length]}.png)` }}
         onClick={onClick}
       >
         <p>{text}</p>
