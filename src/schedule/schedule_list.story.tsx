cd1ac5e3258fef49ed4c2d84e1864f418b90d7c4
liufei <liufei@innobuddy.com>
2018-10-24 18:33:10 +0800

    refs #SMART-4135 schedule component some change

--- src/schedule/schedule_list.story.tsx
+++ src/schedule/schedule_list.story.tsx
@@ -2,13 +2,15 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import ScheduleList from './schedule_list'
 const data = {
-  status: 1,
   name: 'IELTS口语',
   studentName: 'LF1',
   teacherName: 'LF2'
 }
 
 storiesOf('ScheduleList', module)
-  .add('ScheduleList component', () => (
-    <ScheduleList data={data}/>
+  .add('ScheduleList component finished', () => (
+    <ScheduleList data={data} status={3}/>
+  ))
+  .add('ScheduleList component unfinish', () => (
+    <ScheduleList status={1} data={data}/>
   ))
--- src/schedule/schedule_list.tsx
+++ src/schedule/schedule_list.tsx
@@ -4,22 +4,22 @@ import Button from '@material-ui/core/Button'
 
 interface ScheduleListPropsInterface {
   data: {
-    status: number,
     name: string,
     studentName: string,
     teacherName: string
-  }
+  },
+  status: number,
 }
 interface ScheduleListStateInterface {}
 
 export default class ScheduleList extends React.Component<ScheduleListPropsInterface, ScheduleListStateInterface> {
   public render () {
-    const { data } = this.props
+    const { data, status } = this.props
     return (
       <div className='schedule-list'>
         <header>
           <span className='time'>时间</span>
-          <span className='status'>图标{data.status === 3 ? '已完成' : '未完成'}</span>
+          <span className='status'>图标{status === 3 ? '已完成' : '未完成'}</span>
         </header>
         <main>
           <div className='name'>
@@ -32,9 +32,25 @@ export default class ScheduleList extends React.Component<ScheduleListPropsInter
           </div>
         </main>
         <footer>
-          <Button variant='outlined' size='large' color='primary' className='view'>
-            查看反馈
-          </Button>
+          {
+            status === 3 ?
+              <Button variant='outlined' size='large' color='primary' className='view-button'>
+                查看反馈
+              </Button>
+              : <div>
+                <p className='unfinish-button'>
+                  <Button variant='outlined' size='large' color='primary'>
+                    未完成
+                  </Button>
+                </p>
+                <p className='finished-button'>
+                  <Button variant='contained' size='large' color='primary'>
+                    完成
+                  </Button>
+                </p>
+              </div>
+          }
+          
         </footer>
       </div>
     )
