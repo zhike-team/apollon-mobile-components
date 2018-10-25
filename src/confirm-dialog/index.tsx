25f28974fdc761f35b330d58237d98bb85fcf574
wansong <betterofsong@gmail.com>
2018-10-25 21:46:07 +0800

    handle queued requests

--- src/confirm-dialog/index.tsx
+++ src/confirm-dialog/index.tsx
@@ -8,58 +8,135 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import withMobileDialog from '@material-ui/core/withMobileDialog'
 import { WithWidth } from '@material-ui/core/withWidth'
 
+const ACTION_BTN_ID_PREF = 'com.smartstudy.dialog-id-pref'
+
+const ACTION_CANCEL = -1
+const ACTION_AGAIN = -2
+const ACTION_DUP = -3
+
+export interface IOpenDialogAction {
+  text: string
+}
+
+export interface IOpenDialogParams {
+  title: string,
+  message: string,
+  actions?: IOpenDialogAction[]
+}
+
+export interface IOpenDialog {
+  (params: IOpenDialogParams): Promise<number>,
+  instance?: ConfirmDialog,
+  pending?: Promise<number>
+}
+
 export interface ConfirmDialogPropInterface extends React.ClassAttributes<any> {
   fullScreen: boolean
 }
 
 export interface ConfirmDialogStateInterface extends React.ClassAttributes<any> {
-  open: boolean
+  open: boolean,
+  openRequest?: Required<IOpenDialogParams>
 }
 
-class ConfirmDialog extends React.Component<ConfirmDialogPropInterface, ConfirmDialogStateInterface> {
-  state = {
-    open: false
+export class ConfirmDialog extends React.Component<ConfirmDialogPropInterface, ConfirmDialogStateInterface> {
+  callback?: { resolve: (idx: number) => any, reject: (idx: number) => any }
+
+  constructor (props: ConfirmDialogPropInterface) {
+    super(props)
+    openDialog.instance = this
+    this.state = { open: false }
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true })
+  componentWillUnmount () {
+    if (this.callback) {
+      this.callback.resolve(ACTION_CANCEL)
+    }
   }
 
-  handleClose = () => {
-    this.setState({ open: false })
+  open (params: Required<IOpenDialogParams>): Promise<number> {
+    if (this.state.open) {
+      return Promise.reject(ACTION_DUP)
+    }
+
+    return new Promise((resolve, reject) => {
+      this.callback = { resolve, reject }
+      this.setState({ open: true, openRequest: params })
+    })
+  }
+
+  handleClose = (e: React.SyntheticEvent) => {
+    const { id } = e.currentTarget
+    let retCode: number
+    if (!id.startsWith(ACTION_BTN_ID_PREF)) {
+      retCode = ACTION_CANCEL
+    } else {
+      retCode = +id.substring(ACTION_BTN_ID_PREF.length + 1)
+    }
+    this.setState({ open: false }, () => {
+      if (this.callback) {
+        this.callback.resolve(retCode)
+        this.callback = undefined
+      }
+    })
+  }
+
+  renderActions = () => {
+    if (!this.state.openRequest) {
+      return null
+    }
+    const { actions } = this.state.openRequest
+    return (
+      <DialogActions>
+        {actions.map(({ text }, ii) => <Button id={`${ACTION_BTN_ID_PREF}:${ii}`} key={text} onClick={this.handleClose} color='primary' autoFocus={ii === actions.length - 1} >{text}</Button>)}
+      </DialogActions>
+    )
   }
 
   render () {
+    if (!this.state.openRequest) {
+      return null
+    }
     const { fullScreen } = this.props
-
+    const { title, message } = this.state.openRequest
     return (
-      <div>
-        <Button onClick={this.handleClickOpen}>Open responsive dialog</Button>
-        <Dialog
-          fullScreen={fullScreen}
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby='responsive-dialog-title'
-        >
-          <DialogTitle id='responsive-dialog-title'>{"Use Google's location service?"}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Let Google help apps determine location. This means sending anonymous location data to
-              Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color='primary'>
-              Disagree
-            </Button>
-            <Button onClick={this.handleClose} color='primary' autoFocus={true}>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
+      <Dialog
+        id={`${ACTION_BTN_ID_PREF}:-1`}
+        fullScreen={fullScreen}
+        open={this.state.open}
+        onClose={this.handleClose}
+        aria-labelledby='responsive-dialog-title'
+      >
+        <DialogTitle id='responsive-dialog-title'>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{message}</DialogContentText>
+        </DialogContent>
+        {this.renderActions()}
+      </Dialog>
     )
   }
+}
+
+export const openDialog: IOpenDialog = async (params: IOpenDialogParams) => {
+  if (!params.actions) {
+    params.actions = [{ text: '取消' }, { text: '确定' }]
+  }
+  const { instance } = openDialog
+  if (!instance) {
+    return Promise.reject(ACTION_AGAIN)
+  }
+
+  const ret = Promise.resolve(openDialog.pending || 0).then(() => {
+    return instance.open(params as Required<IOpenDialogParams>)
+  })
+  .then(code => {
+    if (openDialog.pending === ret) {
+      openDialog.pending = undefined
+    }
+    return code
+  })
+  openDialog.pending = ret
+  return ret
 }
 
 export default withMobileDialog<ConfirmDialogPropInterface>()(ConfirmDialog)
--- src/confirm-dialog/stories/demo.story.tsx
+++ src/confirm-dialog/stories/demo.story.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
-import ConfirmDialog from '../index'
+import Button from '@material-ui/core/Button'
+import ConfirmDialog, { openDialog } from '../index'
 import theme from '../../theme'
 
+let counter = 0
+async function handleClick () {
+  counter += 2
+  const idxs = await Promise.all([
+    openDialog({
+      title: `title: ${counter - 1}`,
+      message: `message: message: message: message: message: message: ${counter - 1}`
+    }),
+    openDialog({
+      title: `title: ${counter}`,
+      message: `message: ${counter}`
+    })
+  ])
+  console.log(`click id: ${counter - 1}, ${counter}, return: ${idxs[0]}, ${idxs[1]}`)
+}
+
 storiesOf('Confirm Dialog', module)
-  .add('fullscreen', () => (
+  .add('click open', () => (
     <MuiThemeProvider theme={theme}>
-      <ConfirmDialog fullScreen={true} />
-    </MuiThemeProvider>
-  ))
-  .add('not fullscreen', () => (
-    <MuiThemeProvider theme={theme}>
+      <Button id={'123'} onClick={handleClick} >打开</Button>
       <ConfirmDialog fullScreen={false} />
     </MuiThemeProvider>
   ))
