22f7ad8ce9da03e25e9a295ed4326c600526f31e
gx <gaoxiang@innobuddy.com>
2018-11-15 19:13:30 +0800

    ui modification

--- src/theme.ts
+++ src/theme.ts
@@ -6,13 +6,17 @@ declare module '@material-ui/core/styles/createMuiTheme' {
   interface Theme {
     custom: {
       gray: string,
-      gray2: string
+      gray2: string,
+      lightBlue: string,
+      white: string
     }
   }
   interface ThemeOptions {
     custom: {
       gray: string,
-      gray2: string
+      gray2: string,
+      lightBlue: string,
+      white: string
     }
   }
 }
@@ -28,6 +32,8 @@ export default createMuiTheme({
   },
   custom: {
     gray: '#8F9DA5',
-    gray2: '#2E3236'
+    gray2: '#2E3236',
+    lightBlue: '#39F',
+    white: '#FFF'
   }
 })
