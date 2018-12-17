efb6f0866fea0b7ab22f016291b7eae7d6b2490a
liufei <liufei@innobuddy.com>
2018-12-17 15:30:56 +0800

    refs #SMART-4451 some change in mine list

---
+++ src/mine/mine-item.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import '../report-list-item/report-list-item.css'
+import Icon from '../icon'
+
+export interface MineListItemPropsInterface {
+  url: string
+  primaryText: string
+  secondaryText: string
+  onMineClick: () => void
+}
+
+interface MineListItemStateInterface {}
+
+export class MineListItem extends React.Component<MineListItemPropsInterface, MineListItemStateInterface> {
+  public render () {
+    return (
+      <div className='mine-list-item' onClick={this.props.onMineClick}>
+        <div className='icon-box'>
+          <img src={this.props.url} className='icon' />
+        </div>
+        <div className='title-box'>
+          <span className='primary-text'>{this.props.primaryText}</span>
+          <span className='secondary-text'>{this.props.secondaryText}</span>
+        </div>
+        <div className='button'><Icon type='calendar-arrow' className='icon' /></div>
+      </div>
+    )
+  }
+}
+export default MineListItem
